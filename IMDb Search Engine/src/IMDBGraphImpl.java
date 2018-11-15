4d0bfaf3e292eaab35d51d5432527c3fe2bbad11
Winnie Ly <wsly@wpi.edu>
2018-11-15 17:57:19 -0500

    Add documentation to classes/methods

--- IMDb Search Engine/src/IMDBGraphImpl.java
+++ IMDb Search Engine/src/IMDBGraphImpl.java
@@ -3,6 +3,11 @@ import java.util.HashMap;
 import java.util.Scanner;
 import java.io.File;
 
+/**
+ * A class that parses the files of actors and actresses and it builds a graph
+ * of the  corresponding actors/actresses and movies.
+ */
+
 public class IMDBGraphImpl
 {	
 	private final Scanner _actorParser;
@@ -17,25 +22,51 @@ public class IMDBGraphImpl
 		buildGraph();
 	}
 	
+	
+	
 	private void buildGraph()
 	{
 		
 	}
+	
+	/**
+	 * Gets all the actor nodes in the graph.
+	 * @return a collection of all the actor and actress nodes in the graph.
+	 */
 	
 	public Collection<? extends Node> getActors()
 	{
 		return _actorNodes.values();
 	}
 	
+	/**
+	 * Gets all the movie nodes in the graph
+	 * @return a Collection of all the movies in the graph
+	 */
+	
 	public Collection<? extends Node> getMovies()
 	{
 		return _movieNodes.values();
 	}
 	
+	/**
+	 * Returns the actor node having the specified name.
+	 * @param name the name of the requested actor
+	 * @return the actor node associated with the specified name or null
+	 * if no such actor exists.
+	 */
+	
 	public Node getActor(String name)
 	{
 		return _actorNodes.get(name);
 	}
+	
+	/**
+	 * Returns the movie node having the specified name.
+	 * @param name the name of the requested movie
+	 * @return the movie node associated with the specified name or null
+	 * if no such movie exists.
+	 */
 	
 	public Node getMovie(String name)
 	{
--- IMDb Search Engine/src/IMDBNode.java
+++ IMDb Search Engine/src/IMDBNode.java
@@ -1,10 +1,20 @@
 import java.util.Collection;
 import java.util.LinkedList;
 
+/**
+ * A class that gets the name of a specified node and gets all its neighbors nodes.
+ *
+ */
+
 public class IMDBNode implements Node
 {
 	private String _name;
 	private LinkedList<Node> _neighbors;
+	
+	/**
+	 * @param name the name of the specified node in the graph
+	 * @param neighbors the list of nearby nodes in the graph
+	 */
 	
 	public IMDBNode(String name, LinkedList<Node> neighbors)
 	{
@@ -12,20 +22,41 @@ public class IMDBNode implements Node
 		_neighbors = neighbors;
 	}
 	
+	/**
+	 * Returns the name of a node in the graph
+	 * @return the name of a node in the graph
+	 */
+	
 	public String getName()
 	{
 		return _name;
 	}
+	
+	/**
+	 * Returns a collection of nearby nodes in the graph
+	 * @return a collection of nearby nodes in the graph
+	 */
 	
 	public Collection<? extends Node> getNeighbors()
 	{
 		return _neighbors;
 	}
 	
+	/**
+	 * Adds a node to the list of nearby nodes in the graph 
+	 * @param node a node on the graph 
+	 */
+	
 	public void addNeighbor(Node node)
 	{
 		_neighbors.add(node);
 	}
+	
+	/**
+	 * Checks if the requested node in the graph have any neighboring nodes. If it does not
+	 * it should return false. 
+	 * @param node a node on the graph
+	 */
 	
 	public void hasNeighbor(Node node)
 	{
