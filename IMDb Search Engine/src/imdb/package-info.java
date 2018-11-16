d75b97d4ca75920492162c5689bf0b48ecf49b7e
Winnie Ly <wsly@wpi.edu>
2018-11-15 19:25:21 -0500

    Add Javadoc

---
+++ IMDb Search Engine/src/imdb/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Goal is to find a connection between an actor and actress through the 
+ * movies they have starred in. 
+ * 
+ * 
+ * @author Matt Aguiar & Winnie Ly
+ *
+ */
+package imdb;
